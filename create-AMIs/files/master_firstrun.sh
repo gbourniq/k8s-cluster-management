8ddef65534d4788f662fd70a656f121fd68dd3b3
Guillaume Bournique <guillaume.bournique@eigentech.com>
2019-12-19 20:51:08 +0100

    final

--- create-AMIs/files/master_firstrun.sh
+++ create-AMIs/files/master_firstrun.sh
@@ -5,7 +5,7 @@ set -e
 
 # Initialize Kubernetes on the master node
 # --ignore flag to bypass the 2 CPUs requirement
-sudo kubeadm init --ignore-preflight-errors=all # need to extract the kubeadm join command from the output
+sudo kubeadm init --pod-network-cidr=10.244.0.0/16 --ignore-preflight-errors=all # need to extract the kubeadm join command from the output
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
