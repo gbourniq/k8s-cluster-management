bb8a6eb881993c47450e5b2f79a29c2174f5c501
Guillaume Bournique <guillaume.bournique@eigentech.com>
2020-03-03 10:54:42 +0000

    added print join command

--- create-AMIs/files/node_firstrun.sh
+++ create-AMIs/files/node_firstrun.sh
@@ -4,3 +4,6 @@ set -e
 # populate subnet.env file manually
 sudo mkdir /run/flannel/
 sudo mv ~/subnet.env /run/flannel/subnet.env
+
+# if need to get join command, use the following on master node
+# kubeadm token create --print-join-command
